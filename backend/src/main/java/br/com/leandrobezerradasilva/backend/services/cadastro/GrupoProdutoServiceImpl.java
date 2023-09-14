67da7d9923381a2322f7d79417dfb7a42ef0c7a2
Leandro <leandro_ccb10@hotmail.com>
2023-09-14 12:25:16 -0300

    Correcao inserindo a anotacao @Service

--- backend/src/main/java/br/com/leandrobezerradasilva/backend/services/cadastro/GrupoProdutoServiceImpl.java
+++ backend/src/main/java/br/com/leandrobezerradasilva/backend/services/cadastro/GrupoProdutoServiceImpl.java
@@ -4,11 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import br.com.leandrobezerradasilva.backend.models.cadastro.GrupoProduto;
 import br.com.leandrobezerradasilva.backend.repositories.cadastro.GrupoProdutoRepository;
 import br.com.leandrobezerradasilva.backend.services.contracts.GrupoProdutoService;
 
+@Service
 public class GrupoProdutoServiceImpl implements GrupoProdutoService {
 
     @Autowired
