8cc3ee258d1b2930e9d62910caab2337cafee70c
Leandro <leandro_ccb10@hotmail.com>
2023-09-14 16:04:35 -0300

    Adicionado a busca por Id, atualizacao e exclusao

--- backend/src/main/java/br/com/leandrobezerradasilva/backend/controllers/cadastro/GrupoProdutoController.java
+++ backend/src/main/java/br/com/leandrobezerradasilva/backend/controllers/cadastro/GrupoProdutoController.java
@@ -5,8 +5,11 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -28,8 +31,31 @@ public class GrupoProdutoController {
     }
 
     @GetMapping
-    public ResponseEntity<List<GrupoProduto>> listGrupo() {
+    public ResponseEntity<List<GrupoProduto>> listarGrupo() {
         List<GrupoProduto> grupoProdutos = grupoProdutoServiceImpl.findAll();
         return new ResponseEntity<>(grupoProdutos, HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<GrupoProduto> obterPorId(@PathVariable Long id) {
+
+        GrupoProduto grupoProduto = grupoProdutoServiceImpl.findById(id);
+
+        return new ResponseEntity<>(grupoProduto, HttpStatus.OK);
+
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<GrupoProduto> atualizarGrupoProduto(@PathVariable Long id,
+            @RequestBody GrupoProduto grupoProduto) {
+        grupoProduto.setId(id);
+        GrupoProduto grupoProdutoAtualizado = grupoProdutoServiceImpl.update(grupoProduto);
+        return new ResponseEntity<GrupoProduto>(grupoProdutoAtualizado, HttpStatus.OK);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public String excluirGrupoProduto(@PathVariable Long id) {
+        grupoProdutoServiceImpl.deleteById(id);
+        return "Grupo de produto deletado com sucesso!";
     }
 }
