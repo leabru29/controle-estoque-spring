a08f875115e4f108d62896c5ef9481abbbb2a4a0
Leandro <leandro_ccb10@hotmail.com>
2023-09-14 12:29:36 -0300

    Inicio dos testes para Grupo produto

---
+++ backend/src/test/java/br/com/leandrobezerradasilva/backend/services/cadastro/GrupoProdutoServiceImplTest.java
@@ -0,0 +1,51 @@
+package br.com.leandrobezerradasilva.backend.services.cadastro;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+
+import br.com.leandrobezerradasilva.backend.models.cadastro.GrupoProduto;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+public class GrupoProdutoServiceImplTest {
+
+    @Autowired
+    private GrupoProdutoServiceImpl grupoProdutoServiceImpl;
+
+    @Test
+    void testDeleteById() {
+
+    }
+
+    @Test
+    void testFindAll() {
+
+    }
+
+    @Test
+    void testFindById() {
+
+    }
+
+    @Test
+    @DisplayName("Deve salvar um grupo de produto.")
+    void testSave() {
+        GrupoProduto grupoProduto = new GrupoProduto();
+        grupoProduto.setId(null);
+        grupoProduto.setGrupo("Teste");
+        grupoProduto.setAtivo(true);
+
+        grupoProdutoServiceImpl.save(grupoProduto);
+
+        assertEquals(grupoProduto, grupoProduto);
+    }
+
+    @Test
+    void testUpdate() {
+
+    }
+}
