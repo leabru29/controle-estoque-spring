ef3a882e7aa5de8e30d1dfbb034d0f4cc8048f3c
Leandro <leandro_ccb10@hotmail.com>
2023-09-17 14:26:34 -0300

    Iniciando Layout

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './layouts/header/header.component';
+import { FooterComponent } from './layouts/footer/footer.component';
+import { SideNavComponent } from './layouts/side-nav/side-nav.component';
+import { MainComponent } from './layouts/main/main.component';
+import { TopWidgetsComponent } from './layouts/top-widgets/top-widgets.component';
+import { SalesByMonthComponent } from './layouts/sales-by-month/sales-by-month.component';
+import { SalesByCategoryComponent } from './layouts/sales-by-category/sales-by-category.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    SideNavComponent,
+    MainComponent,
+    TopWidgetsComponent,
+    SalesByMonthComponent,
+    SalesByCategoryComponent
   ],
   imports: [
     BrowserModule,
