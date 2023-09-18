7c6a31c22470b5691d25ca31a8108928aa58047b
Leandro <leandro_ccb10@hotmail.com>
2023-09-18 15:56:09 -0300

    Resetando o Angular

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,24 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './layouts/header/header.component';
-import { FooterComponent } from './layouts/footer/footer.component';
-import { SideNavComponent } from './layouts/side-nav/side-nav.component';
-import { MainComponent } from './layouts/main/main.component';
-import { TopWidgetsComponent } from './layouts/top-widgets/top-widgets.component';
-import { SalesByMonthComponent } from './layouts/sales-by-month/sales-by-month.component';
-import { SalesByCategoryComponent } from './layouts/sales-by-category/sales-by-category.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HeaderComponent,
-    FooterComponent,
-    SideNavComponent,
-    MainComponent,
-    TopWidgetsComponent,
-    SalesByMonthComponent,
-    SalesByCategoryComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
