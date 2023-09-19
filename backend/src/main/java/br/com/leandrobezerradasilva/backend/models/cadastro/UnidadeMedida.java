014b93ad64c1a9afb022cc6a464fd00af95c4749
Leandro <leandro_ccb10@hotmail.com>
2023-09-19 12:54:06 -0300

    Validacoes para Unidade de Medida

--- backend/src/main/java/br/com/leandrobezerradasilva/backend/models/cadastro/UnidadeMedida.java
+++ backend/src/main/java/br/com/leandrobezerradasilva/backend/models/cadastro/UnidadeMedida.java
@@ -6,6 +6,9 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -25,12 +28,17 @@ public class UnidadeMedida {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank(message = "O Nome da Unidade deve ser preenchido.")
+    @Size(min = 3, max = 20, message = "O campo deve ter no mínimo 3 caracteres e no máximo 20.")
     @Column(length = 20, unique = true, nullable = false)
     private String nome_unidade;
 
+    @NotBlank(message = "O Nome da Unidade deve ser preenchido.")
     @Column(length = 2, unique = true, nullable = false)
+    @Size(min = 3, max = 20, message = "O campo deve ter no mínimo 3 caracteres e no máximo 20.")
     private String sigla;
 
+    @NotNull(message = "O campo Ativo deve estar preenchido.")
     @Column(nullable = false)
     private Boolean ativo = true;
 }
