a904a84effa7bba0b055ad051217a7d8865f49d2
Leandro <leandro_ccb10@hotmail.com>
2023-09-19 12:48:13 -0300

    Validacao para cadastro de Marca do Produto

--- backend/src/main/java/br/com/leandrobezerradasilva/backend/models/cadastro/MarcaProduto.java
+++ backend/src/main/java/br/com/leandrobezerradasilva/backend/models/cadastro/MarcaProduto.java
@@ -6,6 +6,9 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -25,9 +28,12 @@ public class MarcaProduto {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank(message = "Preencha o campo Marca")
+    @Size(min = 3, max = 50, message = "O campo Marca deve ter no mínimo 3 e no máximo 50 caracteres.")
     @Column(length = 50, nullable = false)
     private String marca;
 
     @Column(nullable = false)
+    @NotNull(message = "O campo Ativo deve estar preenchido.")
     private Boolean ativo = true;
 }
