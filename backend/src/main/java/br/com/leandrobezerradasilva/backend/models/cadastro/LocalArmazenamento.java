2309ad03a7e73aae6b95c2fd48f868537acd07c1
Leandro <leandro_ccb10@hotmail.com>
2023-09-19 12:43:59 -0300

    Validacao para cadastrar Local de armazenamento

--- backend/src/main/java/br/com/leandrobezerradasilva/backend/models/cadastro/LocalArmazenamento.java
+++ backend/src/main/java/br/com/leandrobezerradasilva/backend/models/cadastro/LocalArmazenamento.java
@@ -8,6 +8,9 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -27,9 +30,12 @@ public class LocalArmazenamento {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private UUID id;
 
+    @NotBlank(message = "O campo Nome do Local deve ser Preenchido corretamente.")
+    @Size(min = 3, max = 80, message = "O campo Nome do Local deve ter no mínimo 3 e no máximo 80 caracteres.")
     @Column(length = 80, nullable = false)
     private String nome_local;
 
+    @NotNull(message = "O campo Ativo não deve ser Nulo.")
     @Column(nullable = false)
-    private Boolean ativo;
+    private Boolean ativo = true;
 }
